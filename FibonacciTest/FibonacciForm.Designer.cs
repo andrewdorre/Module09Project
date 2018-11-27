f24a32158380bdd89dc600581e73dc4093a2e699
Andrew Dorre <andrewdorre@gmail.com>
2018-11-26 20:50:57 -0700

    initial

---
+++ FibonacciTest/FibonacciForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace FibonacciTest
+{
+    partial class FibonacciForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(12, 78);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(286, 196);
+            this.outputTextBox.TabIndex = 0;
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Location = new System.Drawing.Point(12, 14);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(289, 13);
+            this.lblHeader.TabIndex = 1;
+            this.lblHeader.Text = "Calls to Fibonacci(46) and Fibonacci(45) in separate threads";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 39);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(286, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Start Asynchronous Fibonacci Calls";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // FibonacciForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 288);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.outputTextBox);
+            this.Name = "FibonacciForm";
+            this.Text = "Fibonacci Sequence";
+            this.Load += new System.EventHandler(this.FibonacciForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
